a37d0a2323855d33e310066f386806f47dff3bad
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2022-06-20 14:08:15 +0900

    誕生日メッセージ変更

--- assets/datepicker/js/liff.js
+++ assets/datepicker/js/liff.js
@@ -1,5 +1,6 @@
 document.getElementById("dp_ok").addEventListener("click", () => {
-  const liffId = "1656766461-gKzBwL6O";
+  // const liffId = "1656766461-gKzBwL6O"; // dev
+  const liffId = "1656653769-LwYV6paV"; // prd
   const yy = document.getElementById("birth-year").value;
   const mm = document.getElementById("birth-month").value;
   const dd = document.getElementById("birth-date").value;
