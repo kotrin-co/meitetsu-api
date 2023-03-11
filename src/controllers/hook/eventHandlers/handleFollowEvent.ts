f226c96d52c23a30dd58d003679be7d5fb339175
kentaro <kentaro523@gmail.com>
2023-03-11 19:09:23 +0900

    開発と本番環境でデプロイを分ける

--- src/controllers/hook/eventHandlers/handleFollowEvent.ts
+++ src/controllers/hook/eventHandlers/handleFollowEvent.ts
@@ -1,29 +1,21 @@
-import { FollowEvent } from "@line/bot-sdk";
-import { client } from "../../../app";
-import LineUser from "../../../models/LineUser";
+import { FollowEvent } from '@line/bot-sdk'
+import { client } from '../../../app'
+import LineUser from '../../../models/LineUser'
 
 const handleFollowEvent = async (event: FollowEvent) => {
-  if (event.type !== "follow") {
-    return;
+  if (event.type !== 'follow') {
+    return
   }
 
-  console.log("followEvent:", event);
+  console.log('followEvent:', event)
 
-  const { replyToken } = event;
-  const lineId = event.source.userId!;
-  const profile = await client.getProfile(lineId);
-  const displayName = profile.displayName;
+  const lineId = event.source.userId!
+  const profile = await client.getProfile(lineId)
+  const displayName = profile.displayName
 
-  const user = new LineUser();
-  const insert = await user.create(lineId, displayName);
-  const message = insert
-    ? `${displayName}さんをDynamoDBへ格納成功`
-    : `${displayName}さんはすでにお友だち登録されています`;
-  console.log(message);
-  await client.replyMessage(replyToken, {
-    type: "text",
-    text: message,
-  });
-};
+  const user = new LineUser()
+  const insert = await user.create(lineId, displayName)
+  console.log('follow insert:', insert)
+}
 
-export default handleFollowEvent;
+export default handleFollowEvent
